46493a6bda1a983ad3654a16b10a60ca972e637f
Brandon Jacobs <jacobs27@byu.edu>
2023-02-23 14:01:20 -0700

    Add project files.

---
+++ Mission8_Group4-9/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission8_Group4_9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
